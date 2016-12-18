3055756d3c82048ffe277142739adbf6a1e7ccc2
Robin Heinemann <robin.ole.heinemann@t-online.de>
2016-12-18 18:39:13 +0100

    add keys

--- deploy.sh
+++ deploy.sh
@@ -56,7 +56,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in pdf_key.enc -out pdf_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../pdf_key.enc -out pdf_key -d
 chmod 600 pdf_key
 eval `ssh-agent -s`
 ssh-add pdf_key
@@ -87,7 +87,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in pages_key.enc -out pages_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../pages_key.enc -out pages_key -d
 chmod 600 pages_key
 eval `ssh-agent -s`
 ssh-add pages_key
