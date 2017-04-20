d62101c9ba950825ebdb7e18b1b575146e068598
Robin Ole Heinemann <robin.ole.heinemann@t-online.de>
2017-04-20 23:03:04 +0200

    add fetch scripts for ubs

---
+++ numerik/tutorial/fetch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pdfs=$(wget -qO- https://typo.iwr.uni-heidelberg.de/groups/mobocon/teaching/numerik-0-ss17/ | grep -o 'href=".*blatt.*pdf"')
+echo $pdfs
+
+for pdf in $pdfs
+do
+	wget --content-disposition "https://typo.iwr.uni-heidelberg.de/$(sed 's/href="//g; s/"//g' <<< $pdf)"
+done  
+find . -size 0 -delete
