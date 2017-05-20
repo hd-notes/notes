3a078e4cc74036cd11afd6d44c952961a083a9de
Robin Ole Heinemann <robin.ole.heinemann@t-online.de>
2017-05-20 12:58:45 +0200

    update

--- ex2/tutorial/fetch.sh
+++ ex2/tutorial/fetch.sh
@@ -4,7 +4,7 @@ IFS=$'\n'
 
 wget --save-cookies cookies.txt --keep-session-cookies --post-data $(cat /home/robin/study/lectures/secrets/moodle) --delete-after https://elearning2.uni-heidelberg.de/login/index.php
 
-pdfs=$(wget --load-cookies cookies.txt -qO- 'https://elearning2.uni-heidelberg.de/course/view.php?id=14332' | pcregrep --buffer-size 10M -o1 '(https:\/\/elearning2\.uni-heidelberg\.de\/mod\/resource\/view\.php\?id=\d*?)"><img src="https:\/\/elearning2\.uni-heidelberg\.de\/theme\/image\.php\/more\/core\/1490094982\/f\/pdf-24" class="iconlarge activityicon" alt=" " role="presentation" \/><span class="instancename">\d*. Übungsblatt')
+pdfs=$(wget --load-cookies cookies.txt -qO- 'https://elearning2.uni-heidelberg.de/course/view.php?id=14332' | pcregrep --buffer-size 10M -o1 '(https:\/\/elearning2\.uni-heidelberg\.de\/mod\/resource\/view\.php\?id=\d*?)"><img src="https:\/\/elearning2\.uni-heidelberg\.de\/theme\/image\.php\/more\/core\/1495103316\/f\/pdf-24" class="iconlarge activityicon" alt=" " role="presentation" \/><span class="instancename">\d*. Übungsblatt')
 
 echo $pdfs
 
